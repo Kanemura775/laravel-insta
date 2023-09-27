6f24890c7f15387062301b5fd5ae2000a2cef2c1
Kanemura775 <milkchocolate0141775@gmail.com>
2023-09-27 20:46:21 +0900

    update

--- config/database.php
+++ config/database.php
@@ -45,12 +45,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'url' => env('DATABASE_URL', 'mysql://t8mnb57b3e54bz7t:xayurd61xygo428n@exbodcemtop76rnz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/g1oj57x8h26qm3j5'),
+            'host' => env('DB_HOST', 'exbodcemtop76rnz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'g1oj57x8h26qm3j5'),
+            'username' => env('DB_USERNAME', 't8mnb57b3e54bz7t'),
+            'password' => env('DB_PASSWORD', 'xayurd61xygo428n'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
@@ -125,7 +125,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
